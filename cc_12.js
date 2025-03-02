8544c2c2378a19ab445319e066e0cbc480dd06ca
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-03-02 17:06:03 -0500

    Task 1 - Adding Comments

--- cc_12.js
+++ cc_12.js
@@ -1,14 +1,18 @@
 // Task 1: Business Dashboard – DOM Element Selection and Creation
 
+// select the dashboard container
 const dashboard = document.getElementById("dashboard");
-
 const selectDashboard = document.querySelector("dashboard");
 
+// create new div
 const metricCard = document.createElement("div");
 
+// assigning class and id to different elements
 metricCard.setAttribute("class", "metric-card");
 metricCard.setAttribute("id", "revenueCard");
 
+// ppulating card with a place holder
 metricCard.textContent = "Revenue: $0";
 
+// append metric card
 dashboard.appendChild(metricCard);
